8914e790411b8fa640126eff896cecd501a3ef94
Sara <sararellano@gmail.com>
2021-01-31 12:33:34 +0100

    feat(mongoose): add mongoose

--- docs/TODO-list/app.js
+++ docs/TODO-list/app.js
@@ -1,7 +1,9 @@
 const express = require('express'),
   bodyParser = require('body-parser'),
-  // Our module: date.js
-  date = require(__dirname + '/date.js'),
+  // Our module: date.js lo borramos porque lo haremos con Mongoose
+  // date = require(__dirname + '/date.js'),
+  mongoose = require('mongoose'),
+  _ = require('lodash'),
   app = express();
 
 // console.log(date());
@@ -13,8 +15,40 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Para añadir el css, lo meto en la carpeta 'public'
 app.use(express.static('public'));
 
-const items = ['Buy food', 'Cook food', 'Eat food'];
-const workItems = [];
+// Los borramos y los agregamos con MongooseDB
+// const items = ['Buy food', 'Cook food', 'Eat food'];
+// const workItems = [];
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true });
+// Creamos schema
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+  name: 'Welcome to your todolist!'
+});
+
+const item2 = new Item({
+  name: 'Hit the + button to add a new item.'
+});
+
+const item3 = new Item({
+  name: '>-- Hit this to delete an item.'
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model('List', listSchema);
+
+
 
 app.get('/', function (req, res) {
 
@@ -27,48 +61,129 @@ app.get('/', function (req, res) {
   //     month: 'long'
   //   },
   //   day = today.toLocaleDateString('en-US', options);
-  const day = date.getDate();
 
+  // Lo borramos porque lo vamos a hacer con Mongoose
+  // const day = date.getDate();
 
 
   // Así se manda un archivo HTML. Pero lo vamos a hacer con EJS, y es res.render
   // res.sendFile(__dirname + "/weekday.html")
   // Sólo se puede agregar 1 res.send, así que si quieres agregar más, pones res.write
   // Se le pasan todas las variables que vayamos a necesitar en list.ejs
-  res.render('list', { listTitle: day, newListItems: items })
+
+  // llamamos a mongo
+  Item.find({}, function (err, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully saved default items to DB');
+        }
+      });
+      res.redirect('/');
+    } else {
+      res.render('list', { listTitle: 'Today', newListItems: foundItems });
+    }
+  });
 
 });
+
+app.get('/:customListName', function (req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect('/' + customListName)
+
+      } else {
+        // show an existing list
+        res.render('list', { listTitle: foundList.name, newListItems: foundList.items });
+
+      }
+    }
+  });
+
+
+});
+
+
+
 
 // FORM
 app.post('/', function (req, res) {
 
   // Coger valor del input para pintarlo en la lista
-  const item = req.body.newItem;
+  // const item = req.body.newItem;
 
   // Si es la página de work, mete los elementos items en el form de work, sino, en home
-  if (req.body.list === 'Work') {
-    workItems.push(item);
-    res.redirect('/work');
-  } else {
-    items.push(item);
+  // if (req.body.list === 'Work') {
+  //   workItems.push(item);
+  //   res.redirect('/work');
+  // } else {
+  //   items.push(item);
+  //   res.redirect('/');
+  // }
+
+
+  const itemName = req.body.newItem,
+    listName = req.body.listBtn,
+    item = new Item({
+      name: itemName
+    });
+
+  if (listName === 'Today') {
+    item.save();
     res.redirect('/');
+
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect('/' + listName);
+    });
+
   }
 
 
 });
 
-// Add Work page
-app.get('/work', function (req, res) {
-  res.render('list', { listTitle: 'Work List', newListItems: workItems });
+// delete items in homepage
+app.post('/delete', function (req, res) {
+  const checkedItemId = req.body.checkboxItem,
+    listName = req.body.listName;
+
+  if (listName === 'Today') {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log('Successfully checked item deleted');
+        res.redirect('/');
+      }
+    });
+
+  } else {
+    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
+      if (!err) {
+        res.redirect('/' + listName);
+      }
+    });
+
+  }
+
 });
 
-app.post('/work', function (req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
 
-  res.redirect('/work');
 
-});
+
 
 
 // Add about page
